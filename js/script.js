867aa2180720758777115b0a7e1f4501f649f22f
unknown <szakalyzs@gmail.com>
2020-11-29 18:52:12 +0100

    Button handling is done.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
 'use strict';
+
+let opArray = [];
+let mixStr = '';
+const display = document.querySelector('.display');
+
+
+(function topBtnHandler() {
+    const operators = document.querySelectorAll('.top__btn');   
+    for (let i = 0; i < operators.length; i++) {
+        operators[i].addEventListener('click', () => {
+            opArray.push(operators[i].textContent);
+            mixStr += operators[i].textContent;
+            display.textContent += operators[i].textContent;
+            console.log(opArray);
+        })
+    }
+})();
+
+(function leftBtnHandler() {
+    const numbers = document.querySelectorAll('.left__btn');
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener('click', () => {
+            mixStr += numbers[i].textContent;
+            display.textContent += numbers[i].textContent;
+            console.log(mixStr);
+        })
+    }
+})();
+
+(function rightBtnHandler() {
+    const equal = document.querySelector('.right__btn');
+    equal.addEventListener('click', () => {
+        display.textContent += equal.textContent;
+    })
+})();
+
+(function clearBtnHandler() {
+    const clear = document.querySelector('.btn--clear');
+    clear.addEventListener('click', () => {
+        display.textContent = '\xa0';
+        opArray = [];
+        mixStr = '';
+    })
+})();
